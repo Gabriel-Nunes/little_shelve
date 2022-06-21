9b1e264e9c9e6fc7c6457e69795c38d1e0a0b2ac
Gabriel-Nunes <gabriel_lima_nunes@hotmail.com>
2022-06-20 22:20:32 -0300

    install.sh update

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #/bin/bash
 
-<<<<<<< HEAD
-=======
-cd little_shelve/
->>>>>>> dev
 sudo apt-get update && sudo apt-get install docker docker-compose
 python3.8 -m venv venv
 source venv/bin/activate
