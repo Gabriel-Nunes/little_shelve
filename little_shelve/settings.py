e7ee0d85f74a10065fd89807e21d77bb2424ca8d
Gabriel-Nunes <gabriel_lima_nunes@hotmail.com>
2022-06-28 22:36:29 -0300

    Production settings

--- little_shelve/settings.py
+++ little_shelve/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://little-shelve.herokuapp.com/']
 
 # Application definition
 INSTALLED_APPS = [
