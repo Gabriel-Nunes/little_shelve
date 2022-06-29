702457099943f0fd445252beebf854db5110324d
Gabriel-Nunes <gabriel_lima_nunes@hotmail.com>
2022-06-28 22:14:22 -0300

    STATIC_ROOT update

--- little_shelve/settings.py
+++ little_shelve/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-# STATICFILES_DIRS = ('/home/gabriel/workspace/little_shelve/templates/static', )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (sent by user)
